de93f15d2e715751180482f3524a33e0418778a8
thinkbruno <brunoramos90@gmail.com>
2026-04-07 12:29:01 -0300

    update StringHelperTest

--- tests/StringHelperTest.php
+++ tests/StringHelperTest.php
@@ -1,11 +1,19 @@
 <?php
 
+namespace Toolbox\Tests;
+
 use PHPUnit\Framework\TestCase;
 use Toolbox\Utils\StringHelper;
 
-class StringHelperTest extends TestCase {
-    public function testSlugifyConvertsTextCorrectly() {
-        $result = StringHelper::slugify("Olá Mundo PHP");
-        $this->assertEquals("ol-mundo-php", $result);
+class StringHelperTest extends TestCase
+{
+    public function test_deve_gerar_slug_corretamente()
+    {
+        $texto = "Aprender PHP é Legal";
+        $esperado = "aprender-php-legal"; // Ajuste conforme sua lógica no StringHelper
+        
+        $resultado = StringHelper::slugify($texto);
+        
+        $this->assertEquals($esperado, $resultado);
     }
 }
